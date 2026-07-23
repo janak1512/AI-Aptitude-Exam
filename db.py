c593d78f7aa6bd20f81bfa1c3728977f9a189513
Janak Kansagara <janaknpatel35@gmail.com>
2026-07-23 22:38:51 +0530

    Add SSL certificate for TiDB

--- db.py
+++ db.py
@@ -11,7 +11,7 @@ def get_db_connection():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
-        ssl_ca="certs/isrgrootx1.pem",
+        ssl_ca=os.path.join("certs", "isrgrootx1.pem"),
     )
 
     return connection
